762c16c8fb06fd65975db36461b2f9758c2c4533
Kevin Orellana <kevin.orellana.89@my.csun.edu>
2021-12-16 16:38:27 -0800

    UI changes, changed initial articles, search bar.
    Changes to CSS, updated the type of article  that loads when the homepage is loaded. Not the top US-trending articles load. I also started working on a search bar.

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -10,12 +10,17 @@ export class NewsApiService {
 
   constructor(private http:HttpClient) { }
   initSources(){
-     return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
   }
   initArticles(){
-   return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+    return this.http.get('https://newsapi.org/v2/top-headlines?country=us&apiKey='+this.api_key);
+   
   }
   getArticlesByID(source: String){
-   return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+  
+  searchArticles(query: String){
+    return this.http.get('https://newsapi.org/v2/everything?q=' + query + '&apiKey='+ this.api_key);
   }
 } 
