a61ad149f6ba3912ac7fdc6831b7fc2be82d9b24
davidcampos713 <91995342+davidcampos713@users.noreply.github.com>
2021-12-17 11:32:55 -0800

    NY Times API added, stories component.
    I connected the NY Times API and created a stories component that displays the NY Times' most viewed articles over the last 7 days.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+
+import { StoriesComponent } from './stories/stories.component';
+
 
 const routes: Routes = [
 
   { path: '',
   pathMatch: 'full',
-  component: HomeComponent}
+  component: AppComponent},
+  {
+    path: 'app-stories', 
+    component: StoriesComponent
+  }
 ];
 
 @NgModule({
